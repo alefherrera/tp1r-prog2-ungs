3d1b925f15e9f34c0639cb38af178756aa1cdbb8
Alejandro <alejandro.herrera@targetamericas.com>
2014-04-23 09:14:05 -0300

    reduccion de complejidad

--- TP1R/src/code/Combinador.java
+++ TP1R/src/code/Combinador.java
@@ -37,29 +37,13 @@ public class Combinador {
        
         public ArrayList<Integer> binario_a_array(String binario){
                 ArrayList<Boolean> v = new ArrayList<Boolean>();
-                for (int i = binario.length()-1; i >= 0; i--) {
-                        v.add(binario.charAt(i) == '1');
-                }
-               
                 ArrayList<Integer> r = new ArrayList<Integer>();
-                for (int i = 0; i < v.size(); i++) {
-                        if (v.get(i))
-                        r.add(elementos.get(i));
+                int max = binario.length()-1;
+                for (int i = max; i >= 0; i--) {
+                        if(binario.charAt(i) == '1')
+                        	r.add(elementos.get(max-i));
                 }
                 return r;
         }
-       
-       
-        @Override
-        public String toString() {
-                // TODO Auto-generated method stub
-                StringBuilder sr = new StringBuilder();
-                for (int i = 0; i < resultados.size(); i++) {
-                        sr.append(resultados.get(i));
-                        sr.append(",");
-                }
-                return sr.toString();
-        }
-       
        
 }
